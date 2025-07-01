4de0fe3397b51f7fde5f782b116be10b9e811861
vadim <drigulawadim@gmail.com>
2025-07-01 15:39:06 +0300

    first commit

---
+++ src/pages/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function Layout() {
+    return (
+        <div>
+            <Link to="/SignIn"><a>Sign In</a></Link>
+        </div>
+    )
+}
+
+export default Layout
